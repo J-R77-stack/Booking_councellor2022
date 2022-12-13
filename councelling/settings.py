4a66e92feec89d99f6e1e7ddcc89a22e795c24cf
J-R77-stack <johnnyramsay81@gmail.com>
2022-12-13 15:49:32 +0000

    new deploy to render

--- councelling/settings.py
+++ councelling/settings.py
@@ -33,8 +33,11 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['councellor2022.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = [ 'localhost']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+  ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
