ef9d40df88f42f676c9bde2b002f256ea66bd887
J-R77-stack <johnnyramsay81@gmail.com>
2022-12-14 18:43:43 +0000

    new deploy to render with bootstrap5

--- councelling/settings.py
+++ councelling/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django-bootstrap5',
+    'bootstrap5',
     'tempus_dominus',
     'crispy_forms',
     'crispy_bootstrap5',
